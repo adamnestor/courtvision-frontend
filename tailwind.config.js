491ff2eb4b4575da63f95754e1078e018c57d686
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-11-21 15:38:30 -0500

    create tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,33 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        success: {
+          DEFAULT: "hsl(var(--success))",
+          foreground: "hsl(var(--success-foreground))",
+        },
+        error: {
+          DEFAULT: "hsl(var(--error))",
+          foreground: "hsl(var(--error-foreground))",
+        },
+        // Other color definitions will be added in step 3
+      },
+    },
   },
-  plugins: [],
-}
-
+  plugins: [require("tailwindcss-animate")],
+};
